deec90beb52cbcba8fd5487e797af5313b762d90
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-06-09 23:03:43 -0600

    adding tasklistitems

--- tests/taskListItem.test.js
+++ tests/taskListItem.test.js
@@ -1,2 +1,53 @@
-node_modules
-tasks.html
+const TaskListItem = require('../lib/TaskListItem.js');
+
+describe('TaskListItem', () => {
+    test('should create a new shape', () => {
+        const taskListItem = new TaskListItem();
+        expect(taskListItem).toEqual(expect.any(Object));
+    }
+});
+
+test('should create a new color', () => {
+    const taskListItem = new TaskListItem();
+    expect(taskListItem).toEqual(expect.any(Object));
+});
+
+test('should create a new border color', () => {
+    const taskListItem = new TaskListItem();
+    expect(taskListItem).toEqual(expect.any(Object));
+});
+
+test('should create a new border width', () => {
+    const taskListItem = new TaskListItem();
+    expect(taskListItem).toEqual(expect.any(Object));
+});
+
+test('should create a new border radius', () => {
+    const taskListItem = new TaskListItem();
+    expect(taskListItem).toEqual(expect.any(Object));
+});
+
+test('should create a new padding', () => {
+    const taskListItem = new TaskListItem();
+    expect(taskListItem).toEqual(expect.any(Object));
+});
+
+test('should create a new margin', () => {
+    const taskListItem = new TaskListItem();
+    expect(taskListItem).toEqual(expect.any(Object));
+});
+
+test('should create a new text color', () => {
+    const taskListItem = new TaskListItem();
+    expect(taskListItem).toEqual(expect.any(Object));
+});
+
+test('should create a new text size', () => {
+    const taskListItem = new TaskListItem();
+    expect(taskListItem).toEqual(expect.any(Object));
+});
+
+test('should create a new text', () => {
+    const taskListItem = new TaskListItem();
+    expect(taskListItem).toEqual(expect.any(Object));
+});

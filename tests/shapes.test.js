6a2c714cc189e1819185d717d239d5c629855c32
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-06-09 22:54:16 -0600

    adding to json

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,55 +1,45 @@
 const {circle,square, triangle, diamond} = require('./shapes');
+const {expect} = require('chai');
+const {spy} = require('sinon'); 
+const {createCanvas} = require('canvas');
+const {writeFileSync} = require('fs');
+const {join} = require('path');
 
-describe('circle', () => {
-    it('should return a circle', () => {
-        expect(circle(4)).toEqual([
-        '   ***   ',
-        ' *     * ',
-        '*       *',
-        '*       *',
-        '*       *',
-        ' *     * ',
-        '   ***   ',
-        ]);
+describe('shapes', () => {
+    describe('circle', () => {
+        it('should draw a circle', () => {
+            const canvas = createCanvas(100, 100);
+            const context = canvas.getContext('2d');
+            circle(context, 50, 50, 50);
+            const buffer = canvas.toBuffer();
+            writeFileSync(join(__dirname, 'circle.png'), buffer);
+        });
     });
-    }
-);
-
-describe('square', () => {
-    it('should return a square', () => {
-        expect(square(4)).toEqual([
-        '****',
-        '*  *',
-        '*  *',
-        '****',
-        ]);
+    describe('square', () => {
+        it('should draw a square', () => {
+            const canvas = createCanvas(100, 100);
+            const context = canvas.getContext('2d');
+            square(context, 50, 50, 50);
+            const buffer = canvas.toBuffer();
+            writeFileSync(join(__dirname, 'square.png'), buffer);
+        });
     });
-    }
-);
-
-describe('triangle', () => {
-    it('should return a upTriangle', () => {
-        expect(upTriangle(4)).toEqual([
-        '   *   ',
-        '  * *  ',
-        ' *   * ',
-        '*     *',
-        ]);
+    describe('triangle', () => {
+        it('should draw a triangle', () => {
+            const canvas = createCanvas(100, 100);
+            const context = canvas.getContext('2d');
+            triangle(context, 50, 50, 50);
+            const buffer = canvas.toBuffer();
+            writeFileSync(join(__dirname, 'triangle.png'), buffer);
+        });
     });
-    }
-);
-
-describe('diamond', () => {
-    it('should return a diamond', () => {
-        expect(diamond(4)).toEqual([
-        '   *   ',
-        '  * *  ',
-        ' *   * ',
-        '*     *',
-        ' *   * ',
-        '  * *  ',
-        '   *   ',
-        ]);
+    describe('diamond', () => {
+        it('should draw a diamond', () => {
+            const canvas = createCanvas(100, 100);
+            const context = canvas.getContext('2d');
+            diamond(context, 50, 50, 50);
+            const buffer = canvas.toBuffer();
+            writeFileSync(join(__dirname, 'diamond.png'), buffer);
+        });
     });
-    }
-);
+});
